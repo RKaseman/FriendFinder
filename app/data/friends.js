ab2a4e174c231d15c92baf41adbfd3579e85516c
Rane M Kaseman <raykman@comcast.net>
2018-09-08 22:17:26 -0600

    2217

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,7 @@
 
 var surveyStore = [{
-    "fName": "test",
-    "photo": "https://github.com/RKaseman",
-    "scores": [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10
-        ]
+    fName: "test",
+    photo: "https://github.com/RKaseman",
+    scores: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
     }];
 
--- server.js
+++ server.js
@@ -3,47 +3,73 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var http = require("http");
-var fs = require("request");
+var fs = require("fs");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var router = express.Router();
 
-router.get('/', function (request, response) {
-    response.json({ message: "successful test" });
-});
+// app.get("/api/friends", function (request, response) {
+//     var fName = request.param("fName");
+//     var photo = request.param("photo");
+//     var scores = request.param("scores");
+
+//     res.send(fName);
+// });
+
+
+// var router = express.Router();
+
+
+// router.get("/", function (request, response) {
+//     response.json({ message: "successful test" });
+// });
 
 
 // ##############################################
+// routes will go here
+app.get("/api/users", function (request, response) {
+    var user_id = request.query.id;
+    var token = request.query.token;
+    var geo = request.query.geo;
+    var test = request.query.test;
+
+    response.send(user_id + " " + token + " " + geo + " " + test);
+});
+// start the server
+app.listen(PORT);
+console.log("Server started! At http://localhost:" + PORT);
+// ##############################################
 
 // Dependencies
-var http = require("http");
-var fs = require("fs");
+// var http = require("http");
+// var fs = require("fs");
 
 // Set our port to 8080
-var PORT = 8080;
+// var PORT = 8080;
 
 // Create our server
-var server = http.createServer(handleRequest);
+// var server = http.createServer(handleRequest);
 
 // Create a function for handling the requests and responses coming into our server
-function handleRequest(req, res) {
+// function handleRequest(req, res) {
 
     // Here we use the fs package to read our index.html file
-    fs.readFile(__dirname + "/index.html", function (err, data) {
+    // fs.readFile(__dirname + "/index.html", function (err, data) {
 
         // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
         // an html file.
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    });
-}
+        // res.writeHead(200, { "Content-Type": "text/html" });
+        // res.end(data);
+    // });
+// }
 
 // Starts our server
-server.listen(PORT, function () {
-    console.log("Server is listening on PORT: " + PORT);
-});
+// server.listen(PORT, function () {
+    // console.log("Server is listening on PORT: " + PORT);
+// });
+
