359380c86352cc12fd3acc1cc42d5bf177547bc7
Rane M Kaseman <raykman@comcast.net>
2018-09-10 00:25:12 -0600

    0025

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,50 +2,50 @@
 var friends = [
     {
         fName: "test1",
-        photo: "https://github.com/RKaseman/RKaseman.github.io/blob/master/assets/images/20170402_013852sR3.jpg",
+        photo: "https://raw.githubusercontent.com/RKaseman/RKaseman.github.io/master/assets/images/20170402_013852sR3.jpg",
         scores: [
-            5,
-            3,
-            5,
-            1,
-            5,
-            1,
-            5,
-            1,
-            1,
-            4
+            "5",
+            "3",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "1",
+            "4"
         ]
     },
     {
         fName: "test2",
-        photo: "https://github.com/RKaseman/RKaseman.github.io/blob/master/assets/images/20170402_013852sR3.jpg",
+        photo: "https://raw.githubusercontent.com/RKaseman/RKaseman.github.io/master/assets/images/20170402_013852sR3.jpg",
         scores: [
-            5,
-            3,
-            5,
-            1,
-            5,
-            1,
-            5,
-            1,
-            1,
-            3
+            "5",
+            "3",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "1",
+            "3"
         ]
     },
     {
         fName: "test3",
-        photo: "https://github.com/RKaseman/RKaseman.github.io/blob/master/assets/images/20170402_013852sR3.jpg",
+        photo: "https://raw.githubusercontent.com/RKaseman/RKaseman.github.io/master/assets/images/20170402_013852sR3.jpg",
         scores: [
-            5,
-            3,
-            5,
-            1,
-            5,
-            1,
-            5,
-            1,
-            1,
-            2
+            "5",
+            "3",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "1",
+            "2"
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 
+// access to friends.js
 var friends = require("../data/friends.js");
 
 
@@ -9,7 +10,47 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(request, response) {
-        friends.push(request.body);
+
+        // var for best match object to display after submit
+        // is match.scoreDif
+        var match = {
+            fName: "",
+            photo: "",
+            scoreDif: 100
+        };
+
+        console.log(request.body);
+
+        // request.body from user app.post
+        var userData = request.body;
+        // userScores from .body objects
+        var userScores = userData.scores;
+
+        console.log(userScores);
+
+        // calculated difference between users
+        var totalDifference = 0;
+
+        // loop through all friends arrays
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+            totalDifference = 0;
+            // loop through each friends score array
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+                // scores must be absolute, so Math.abs()
+                // difference of current user scores and all user scores
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                // is the current user a better match than any existing
+                if (totalDifference <= match.scoreDif) {
+                    match.fName = friends[i].name;
+                    match.photo = friends[i].photo;
+                    match.scoreDif = totalDifference;
+                }
+            }
+        }
+        friends.push(userData);
+        response.json(match);
     });
 
 };
